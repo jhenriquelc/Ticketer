9283af0c5c6a4ec5f53c70c77afde371c465e81f
João H. L. Corrêa <jcorrea.2005@alunos.utfpr.edu.br>
2025-08-21 16:43:39 -0300

    Adicionado Spring Boot ao projeto

---
+++ src/main/java/br/dev/casa24h/ticketer/TicketerApplication.java
@@ -0,0 +1,13 @@
+package br.dev.casa24h.ticketer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/casa24h/ticketer/TicketerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.casa24h.ticketer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
