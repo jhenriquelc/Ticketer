14e6e0966483b4a576c623e7f88df214f9459522
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2025-10-22 16:42:08 -0300

    Views

---
+++ src/main/resources/static/js/sidebar.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const items = document.querySelectorAll(".menu-item > p");
+
+  items.forEach(item => {
+    item.addEventListener("click", () => {
+      const submenu = item.nextElementSibling;
+      const isVisible = submenu.style.display === "block";
+
+      document.querySelectorAll(".submenu").forEach(s => s.style.display = "none");
+      submenu.style.display = isVisible ? "none" : "block";
+    });
+  });
+});
