cb000630e13ecfff5f3a9c7273b29579beef7737
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2025-11-26 16:51:39 -0300

    adicionando o homecontroller

---
+++ src/main/java/br/dev/casa24h/ticketer/controller/HomeController.java
@@ -0,0 +1,44 @@
+package br.dev.casa24h.ticketer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirecionarParaVendas() {
+        return "redirect:/ingressos";
+    }
+
+    @GetMapping("/ingressos")
+    public String ingressos(Model model) {
+        model.addAttribute("conteudo", "ingressos");
+        return "index";
+    }
+
+    @GetMapping("/bomboniere")
+    public String bomboniere(Model model) {
+        model.addAttribute("conteudo", "bomboniere");
+        return "index";
+    }
+
+    @GetMapping("/filmes")
+    public String filmes(Model model) {
+        model.addAttribute("conteudo", "filmes");
+        return "index";
+    }
+
+    @GetMapping("/sessoes")
+    public String sessoes(Model model) {
+        model.addAttribute("conteudo", "sessoes");
+        return "index";
+    }
+
+    @GetMapping("/gerenciamento-bomboniere")
+    public String gerenciamentoBomboniere(Model model) {
+        model.addAttribute("conteudo", "gerenciamento-bomboniere");
+        return "index";
+    }
+}
