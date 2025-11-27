056b829c50a46ae76a1c384e7b5a9e21867bd29c
GabrielAlvesDeOliveira <alvesg2003@gmail.com>
2025-11-26 23:58:55 -0300

    wip

--- src/main/java/br/dev/casa24h/ticketer/controller/HomeController.java
+++ src/main/java/br/dev/casa24h/ticketer/controller/HomeController.java
@@ -1,11 +1,31 @@
 package br.dev.casa24h.ticketer.controller;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.dev.casa24h.ticketer.model.*;
+import br.dev.casa24h.ticketer.repository.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Controller
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    ItemBomboniereRepository itemBomboniereRepository;
+
+    @Autowired
+    ExibicaoRepository exibicaoRepository;
+
+    @Autowired
+    FilmeRepository filmeRepository;
 
     @GetMapping("/")
     public String redirecionarParaVendas() {
@@ -14,13 +34,30 @@ public class HomeController {
 
     @GetMapping("/ingressos")
     public String ingressos(Model model) {
+        
+        Iterable<Filme> filmes = filmeRepository.findAll();
+        Map<Filme, List<Exibicao>> novoFilmes = new HashMap<>();
+
+        for (Filme filme : filmes) {
+            List<Exibicao> exibicoesDoFilme = exibicaoRepository.findByFilmeId(filme.getId());
+            novoFilmes.put(filme, exibicoesDoFilme);
+        }
+
+        logger.info(novoFilmes.toString());
+        logger.info("Total de filmes encontrados: " + novoFilmes.size());
+        // logger.info(novoFilmes);
+        logger.info(filmes.toString());
         model.addAttribute("conteudo", "ingressos");
+        // model.addAttribute("Filmes", filmeRepository.findAll());
+        model.addAttribute("Filmes", novoFilmes);
         return "index";
     }
 
     @GetMapping("/bomboniere")
     public String bomboniere(Model model) {
+        
         model.addAttribute("conteudo", "bomboniere");
+        model.addAttribute("itensBomboniere", itemBomboniereRepository.findAll());
         return "index";
     }
 
