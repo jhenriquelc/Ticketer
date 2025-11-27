0ec1cb74b9f05ed221eb4884093ceada66c37994
João H. L. Corrêa <jcorrea.2005@alunos.utfpr.edu.br>
2025-11-26 21:02:49 -0300

    Adicionar funcionalidade de cadastro de item de bomboniere

--- src/main/java/br/dev/casa24h/ticketer/controller/HomeController.java
+++ src/main/java/br/dev/casa24h/ticketer/controller/HomeController.java
@@ -1,11 +1,18 @@
 package br.dev.casa24h.ticketer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.dev.casa24h.ticketer.model.ItemBomboniere;
+import br.dev.casa24h.ticketer.repository.ItemBomboniereRepository;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    ItemBomboniereRepository itemBomboniereRepository;
 
     @GetMapping("/")
     public String redirecionarParaVendas() {
@@ -21,6 +28,7 @@ public class HomeController {
     @GetMapping("/bomboniere")
     public String bomboniere(Model model) {
         model.addAttribute("conteudo", "bomboniere");
+        model.addAttribute("itens", itemBomboniereRepository.findAll());
         return "index";
     }
 
@@ -39,6 +47,7 @@ public class HomeController {
     @GetMapping("/gerenciamento-bomboniere")
     public String gerenciamentoBomboniere(Model model) {
         model.addAttribute("conteudo", "gerenciamento-bomboniere");
+        model.addAttribute("itemBomboniere", new ItemBomboniere("", 0f));
         return "index";
     }
 }
